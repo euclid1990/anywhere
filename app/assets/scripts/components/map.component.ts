f2184bb186b66e4772530174e668512aa21c743c
Nguyen Van Vuong <nguyen.van.vuong@framgia.com>
2016-04-10 18:04:04 +0700

    Add google map API

--- app/assets/scripts/components/map.component.ts
+++ app/assets/scripts/components/map.component.ts
@@ -1,15 +1,47 @@
-import {Component} from 'angular2/core';
+import {Component, EventEmitter, Input, Inject, ElementRef} from 'angular2/core';
+import {OnInit, AfterViewInit} from 'angular2/core';
+import {Observable} from 'rxjs/Rx';
+import * as types from '../types';
+import {DatabaseService} from '../services/database.service';
+import {StorageService} from '../services/storage.service';
+import {EmitterService} from '../services/emitter.service';
+
+declare var $: any;
+declare var document: any;
+declare var google: any;
 
 @Component({
     selector: 'div[name=map]',
     templateUrl: '/partials/map.html',
 })
 
-export class MapComponent {
+export class MapComponent implements OnInit, AfterViewInit {
 
-    constructor() {
+    public mapId: string = 'map';
+    public map: any;
+
+    constructor(public elementRef: ElementRef,
+                public dbService: DatabaseService,
+                public storageService: StorageService) {
 
     }
+
+    ngOnInit() {
+        let self = this;
+        self.initMap();
+    }
+
+    ngAfterViewInit() {
+        let self = this;
+    }
+
+    initMap() {
+        let self = this;
+        this.map = new google.maps.Map(document.getElementById(self.mapId), {
+          center: {lat: 21.0227732, lng: 105.8019441},
+          zoom: 12
+        });
+     }
 
     getLocation() {
 
